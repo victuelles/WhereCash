f0f1ddce2261a168fb74534d9650a971edd4077f
Rom Vic <technoyguy@gmail.com>
2018-07-31 12:33:07 -0700

    code ckean up

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -6,12 +6,12 @@ class EventList extends Component {
     const { events, onEventOpen, deleteEvent } = this.props;
     return (
       <div>
-        <h1>Event List</h1>
+
         {events.map(event => (
           <EventListItem
             key={event.id}
             event={event}
-            onEventOpen={onEventOpen}
+          
             deleteEvent={deleteEvent}
           />
         ))}
