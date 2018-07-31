3feb2e3714ea3a9d860d05bcaa25a25827cdf5a8
Rom Vic <technoyguy@gmail.com>
2018-07-30 22:43:47 -0700

    event list added

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-       Revents
-      </div>
+
+        <Container className="main">
+          <NavBar/>
+          <h1>Where.Cash</h1>
+         <EventDashboard/>
+        </Container>
+
     );
   }
 }
 
 export default App;
-
