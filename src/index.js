100f2e15a5b5b33de3778fe364922971562d42d0
Rom Vic <technoyguy@gmail.com>
2018-07-31 10:39:17 -0700

    test redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
 
-const rootEl= document.getElementById('root')
+const store = configureStore();
 
-let render=()=>{
-    ReactDOM.render(<App />,rootEl)
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
-if(module.hot){
-    module.hot.accept('./app/layout/App',()=>{
-        setTimeout(render)
-
-    })
-}
-render()
+render();
 
 registerServiceWorker();
